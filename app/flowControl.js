7fe2ca12156a0449209cd884e330c90c9778dfbe
Marcus Tenório <marcusvtenorio@gmail.com>
2015-12-07 16:14:52 -0200

    Make bestPractices, count, flowControl and functions tests pass

--- app/flowControl.js
+++ app/flowControl.js
@@ -11,5 +11,14 @@ exports.flowControlAnswers = {
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
 
+    if (typeof num === 'undefined' || isNaN(num)) return false;
+
+    var result = '';
+
+    if (num % 3 === 0) result = 'fizz';
+    if (num % 5 === 0) result += 'buzz';
+    if (result === '') result = num;
+
+    return result;
   }
 };
--- app/functions.js
+++ app/functions.js
@@ -2,38 +2,74 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+    return fn.apply(null, arr);
   },
 
   speak : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   functionFunction : function(str) {
-
+    return function(str2) {
+      return str + ', ' + str2;
+    }
   },
 
   makeClosures : function(arr, fn) {
-
+    return arr.map(function(el) {
+      return function() {
+        return fn(el);
+      };
+    });
   },
 
   partial : function(fn, str1, str2) {
-
+    return function(punctuation) {
+      return fn(str1, str2, punctuation);
+    };
   },
 
   useArguments : function() {
+    var sum = 0;
 
+    for (var i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+    return sum;
   },
 
   callIt : function(fn) {
-
+    var args = Array.prototype.slice.call(arguments, 1);
+    fn.apply(null, args);
   },
 
   partialUsingArguments : function(fn) {
+    var args = Array.prototype.slice.call(arguments, 1);
 
+    return function() {
+      var moreArgs = Array.prototype.slice.call(arguments);
+      return fn.apply(null, args.concat(moreArgs));
+    };
   },
 
   curryIt : function(fn) {
+    function applyArguments(fn, arguments) {
+      return fn.apply(null, arguments);
+    }
 
+    function getArgumentAccumulator(accumulatedArguments, expectedArgumentsCount) {
+      return function (currentArgument) {
+        accumulatedArguments.push(currentArgument);
+
+        var allArgumentsProvided = accumulatedArguments.length === expectedArgumentsCount;
+
+        if (allArgumentsProvided) {
+          return applyArguments(fn, accumulatedArguments);
+        }
+        return getArgumentAccumulator(accumulatedArguments, expectedArgumentsCount);
+      };
+    }
+
+    return getArgumentAccumulator([], fn.length);
   }
 };
