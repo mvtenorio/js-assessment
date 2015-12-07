0625eda1f5f3e55135c84a9d1a9f6559960fd211
Marcus Tenório <marcusvtenorio@gmail.com>
2015-12-06 22:42:00 -0200

    Make async.js tests pass

--- app/async.js
+++ app/async.js
@@ -2,10 +2,36 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function(resolve) {
+      setTimeout(function() {
+        resolve(value);
+      }, 10);
+    });
   },
 
   manipulateRemoteData : function(url) {
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
 
+      req.onload = function() {
+        if (req.status == 200) {
+          resolve(req.response);
+        }
+        else {
+          reject(Error(req.statusText));
+        }
+      }
+      req.send();
+    })
+      .then(JSON.parse)
+      .then(function(data) {
+        return data.people.map(function(el) {
+          return el.name;
+        });
+      })
+      .then(function(data) {
+        return data.sort();
+      });
   }
 };
