30777233cfdc05d08cf54e3f8556cc808fa96188
Marcus Tenório <marcusvtenorio@gmail.com>
2016-03-26 17:39:29 -0300

    Make logicalOperators, modules and objects tests pass

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
+    var props = [];
 
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        props.push(prop + ': ' + obj[prop]);
+      }
+    }
+    return props;
   }
 };
