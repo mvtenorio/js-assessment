4c2b1a22500e01225401c799ec70f546f6970518
Marcus Tenório <marcusvtenorio@gmail.com>
2016-03-25 23:04:41 -0300

    Refactor array function using a more functional approach

--- app/arrays.js
+++ app/arrays.js
@@ -1,104 +1,64 @@
-exports = (typeof window === 'undefined') ? global : window;
+exports = (typeof window === 'undefined') ? global : window
 
 exports.arraysAnswers = {
 
-  indexOf : function(arr, item) {
-    return arr.indexOf(item);
-  },
+  indexOf : (arr, item) => arr.indexOf(item),
 
-  sum : function(arr) {
-    var sum = 0;
+  sum : (arr) => arr.reduce((a, b) => a + b),
 
-    for (var i = 0; i < arr.length; i++) {
-      sum += arr[i];
-    }
-    return sum;
-  },
+  remove : (arr, item) => arr.filter((el) => el !== item),
 
-  remove : function(arr, item) {
-    var index = arr.indexOf(item);
+  removeWithoutCopy : (arr, item) => {
+    let index = arr.indexOf(item)
 
     while (index > -1) {
-      arr.splice(index, 1);
-      index = arr.indexOf(item);
+      arr.splice(index, 1)
+      index = arr.indexOf(item)
     }
-    return arr;
+    return arr
   },
 
-  removeWithoutCopy : function(arr, item) {
-    return this.remove(arr, item);
+  append : (arr, item) => [...arr, item],
+
+  truncate : (arr) => arr.slice(0, arr.length - 1),
+
+  prepend : (arr, item) => [item, ...arr],
+
+  curtail : (arr) => arr.slice(1),
+
+  concat : (arr1, arr2) => [...arr1, ...arr2],
+
+  insert : (arr, item, index) => {
+    return [
+      ...arr.slice(0, index),
+      item,
+      ...arr.slice(index)
+    ]
   },
 
-  append : function(arr, item) {
-    // returns the new length
-    arr.push(item);
-    return arr;
-  },
+  count : (arr, item) => arr.filter((el) => el === item).length,
 
-  truncate : function(arr) {
-    // returns the element that was removed
-    arr.pop();
-    return arr;
-  },
+  duplicates : (arr) => {
+    let dup = []
 
-  prepend : function(arr, item) {
-    // returns the new length
-    arr.unshift(item);
-    return arr;
-  },
-
-  curtail : function(arr) {
-    // returns the element that was removed
-    arr.shift();
-    return arr;
-  },
-
-  concat : function(arr1, arr2) {
-    // returns a new array
-    return arr1.concat(arr2);
-  },
-
-  insert : function(arr, item, index) {
-    // returns [] since no item was deleted
-    arr.splice(index, 0, item);
-    return arr;
-  },
-
-  count : function(arr, item) {
-    return arr.filter(function(val) {
-      return (val === item);
-    }).length;
-  },
-
-  duplicates : function(arr) {
-    arr.sort();
-    var duplicates = [];
-
-    for (var i = 1; i < arr.length; i++) {
-      var current = arr[i];
-      var previous = arr[i-1];
-
-      if (current === previous && duplicates.indexOf(current) === -1) {
-        duplicates.push(current);
+    arr.forEach((el, index) => {
+      if (arr.includes(el, index + 1) && !dup.includes(el)) {
+        dup = [...dup, el]
       }
-    }
-    return duplicates;
+    })
+    return dup
   },
 
-  square : function(arr) {
-    return arr.map(function(el) {
-      return el * el;
-    });
-  },
+  square : (arr) => arr.map((el) => el * el),
 
-  findAllOccurrences : function(arr, target) {
-    var ocurrences = [];
+  findAllOccurrences : (arr, target) => {
+    let oc = []
 
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === target) {
-        ocurrences.push(i);
+    arr.forEach((el, index) => {
+      if (el === target) {
+        oc = [...oc, index]
       }
-    }
-    return ocurrences;
+    })
+    return oc;
   }
-};
+}
